f544203c0fedd02f36f7c8624f746e9dbc9108c3
ajayjury <ajay@jurysoft.com>
2022-02-16 21:37:38 +0530

    api https updated

--- constant.js
+++ constant.js
@@ -1,6 +1,8 @@
 const constant = {
-    api_route: 'http://35.154.209.18/api',
-    api_image_route: 'http://35.154.209.18/storage'
+    api_route: 'https://cotton-culture.com/api',
+    api_image_route: 'https://cotton-culture.com/storage'
+    // api_route: 'http://35.154.209.18/api',
+    // api_image_route: 'http://35.154.209.18/storage'
 }
 
 export default constant;
